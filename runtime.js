1f92a5555bac2783b653d58b6f02f82e6860dd19
notsniped <72265661+notsniped@users.noreply.github.com>
2021-11-26 09:31:13 +0530

    Update runtime.js
    Added support for new soundtracks

--- runtime.js
+++ runtime.js
@@ -1,10 +1,12 @@
 song = '';
+song2 = '';
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 function preload() {
     song = loadSound('music.mp3');
+    song2 = loadSound('music2.mp3');
 }
 function setup() {
     canvas = createCanvas(600, 500);
