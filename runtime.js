f339a6505304ca8a66efecc85e39222055671043
notsniped <72265661+notsniped@users.noreply.github.com>
2021-11-28 11:42:30 +0530

    Update runtime.js

--- runtime.js
+++ runtime.js
@@ -4,6 +4,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreleftWrist = 0;
 function preload() {
     song = loadSound('music.mp3');
     song2 = loadSound('music2.mp3');
@@ -19,11 +20,11 @@ function setup() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        scoreleftWrist = results[0].pose.keypoints[9].score;
         // Left Wrist
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log('LeftWrist X = ' + leftWristX + '; LeftWrist Y = ' + leftWristY);
-
         // Right Wrist
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
@@ -35,6 +36,16 @@ function modelLoaded() {
 }
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill('#FF0000');
+    stroke('#FF0000');
+    if (scoreleftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimal = floor(InNumberleftWristY);
+        volume = remove_decimal/1000;
+        document.getElementById('volume').innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 function play() {
     song.play();
